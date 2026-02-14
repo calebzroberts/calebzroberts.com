1b77c062f1ea51d9c76575beeb571758ce5ca42a
calebzroberts <caleb.roberts798@gmail.com>
2026-02-14 18:58:39 -0500

    updated home and work experience styling

--- javascript/mobile-menu.js
+++ javascript/mobile-menu.js
@@ -1,11 +1,17 @@
 const menuToggle = document.getElementById("menu-toggle");
-  const closeMenu = document.getElementById("close-menu");
-  const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeMenu = document.getElementById("close-menu");
 
-  menuToggle.addEventListener("click", () => {
-    mobileMenu.classList.add("active");
-  });
+menuToggle.addEventListener("click", () => {
+    mobileMenu.style.opacity = "1";
+    mobileMenu.style.visibility = "visible";
+    mobileMenu.style.pointerEvents = "auto";
+    mobileMenu.style.transform = "translateY(0)";
+});
 
-  closeMenu.addEventListener("click", () => {
-    mobileMenu.classList.remove("active");
-  });
+closeMenu.addEventListener("click", () => {
+    mobileMenu.style.opacity = "0";
+    mobileMenu.style.visibility = "hidden";
+    mobileMenu.style.pointerEvents = "none";
+    mobileMenu.style.transform = "translateY(-50px)";
+});
